78076d346f8d26f6e0c1f2888cc97835fa49f649
Coleman Broaddus <coleman.broaddus@gmail.com>
2023-02-18 23:56:18 -0500

    Improved workflow for inspecting png folders of training data.

--- inspect_data.sh
+++ inspect_data.sh
@@ -1,13 +1,23 @@
 mkdir inspect_data
+rm -rf inspect_data/*
 
-for alldirs in /Users/broaddus/Desktop/mpi-remote/project-broaddus/cpnet3/cpnet-out/*; do
+for alldirs in /Users/broaddus/Desktop/mpi-remote/project-broaddus/cpnet3/cpnet-out/*
+do
 	isbiname=$(basename $alldirs)
 	echo $isbiname
 	basedir="/Users/broaddus/Desktop/mpi-remote/project-broaddus/cpnet3/cpnet-out/$isbiname/data/png/"
-	for img in $basedir/*; do
-		name="inspect_data/$isbiname-$(basename $img)"
-		printf "$name \n"
-		cp $img $name
-	done;
-done;
+	
+	## if directory exists and isn't empty
+	if [ -d $basedir ] && [ ! -z "$(ls -A $basedir)" ]
+	then
+		targetdir="inspect_data/$isbiname/"
+		mkdir -p $targetdir
+		for img in $basedir/*
+		do
+			name=$targetdir/$(basename $img)
+			printf "$name \n"
+			cp $img $name
+		done
+	fi
+done
 
